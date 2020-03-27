f9f744e6713c7b10e448fe9b6b3ac2baa7a8ba0c
Gian <giancarlo.amaral@pixter.com.br>
2020-03-27 20:11:19 -0300

    adicionado select de modelos

--- dist/js/actions.js
+++ dist/js/actions.js
@@ -4,20 +4,34 @@ fipeFormulario.reset();
 
 var selectCategoria = document.querySelector("#categoria");
 var selectMarca = document.querySelector("#marca");
+var selectModelo = document.querySelector("#modelo");
 
 selectCategoria.addEventListener("change", buscarMarcas);
 selectMarca.addEventListener("change", buscarModelos);
 
-function buscarMarcas(evento) {
-  var categoria = evento.target.value;
-  selectMarca.disabled = true;
-  selectMarca.innerHTML = "";
+var categoria = "";
+var marca = "";
+var modelo = "";
+var ano = "";
 
+function criarOpcaoPadrao(texto) {
   var defaultOption = document.createElement("option");
   defaultOption.value = "";
   defaultOption.hidden = true;
   defaultOption.selected = "selected";
-  defaultOption.innerHTML = "Selecione a marca";
+  defaultOption.innerHTML = texto;
+
+  return defaultOption;
+}
+
+function buscarMarcas(evento) {
+  categoria = evento.target.value;
+  selectMarca.disabled = true;
+  selectModelo.disabled = true;
+  selectMarca.innerHTML = "";
+
+  defaultOption = criarOpcaoPadrao("Selecione a Marca");
+
   selectMarca.appendChild(defaultOption);
 
   axios
@@ -30,13 +44,36 @@ function buscarMarcas(evento) {
 
         selectMarca.appendChild(option);
       });
+      selectMarca.disabled = false;
     });
-
-  selectMarca.disabled = false;
 }
 
 function buscarModelos(evento) {
-  var modelo = evento.target.value;
+  modelo = evento.target.value;
+  selectModelo.disabled = true;
+  selectModelo.innerHTML = "";
 
-  console.log(modelo);
+  defaultOption = criarOpcaoPadrao("Selecione o modelo");
+
+  selectModelo.appendChild(defaultOption);
+
+  axios
+    .get(
+      "https://parallelum.com.br/fipe/api/v1/" +
+        categoria +
+        "/marcas/" +
+        modelo +
+        "/modelos"
+    )
+    .then(function(resposta) {
+      console.log(resposta);
+      resposta.data.modelos.map(function(modelo) {
+        var option = document.createElement("option");
+        option.innerHTML = modelo.nome;
+        option.value = modelo.codigo;
+
+        selectModelo.appendChild(option);
+      });
+      selectModelo.disabled = false;
+    });
 }
