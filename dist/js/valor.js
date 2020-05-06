f617be5b043047581ac7668f993b67b9967cf1b8
Giancarlo <giancarlo.tutini@hotmail.com>
2020-05-06 20:49:10 -0300

    completa primeira parte da pagina de valores

--- dist/js/valor.js
+++ dist/js/valor.js
@@ -1,6 +1,11 @@
-function init() {
+function buscarValor() {
   var params = window.location.search;
   var query = new URLSearchParams(params);
+  var veiculo = document.getElementById("veiculo");
+  var valor = document.getElementById("valor");
+  var marca = document.getElementById("marca");
+  var ano = document.getElementById("ano");
+  var combustivel = document.getElementById("combustivel");
 
   axios
     .get(
@@ -13,11 +18,20 @@ function init() {
         "/anos/" +
         query.get("ano")
     )
-    .then(function(resposta) {
-      console.log(resposta);
+    .then(function (resposta) {
+      console.log(resposta.data);
+      textoVeiculo = resposta.data.Modelo;
+      textoValor = "Valor: " + resposta.data.Valor;
+      textoMarca = "Marca: " + resposta.data.Marca;
+      textoAno = "Ano: " + resposta.data.AnoModelo;
+      textoCombustivel = "Combustível: " + resposta.data.Combustivel;
+
+      veiculo.innerText = textoVeiculo;
+      valor.innerText = textoValor;
+      marca.innerText = textoMarca;
+      ano.innerText = textoAno;
+      combustivel.innerText = textoCombustivel;
     });
 }
 
-function buscarValor() {}
-
-init();
+buscarValor();
